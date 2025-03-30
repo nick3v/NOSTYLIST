2a64c4a6f6c06481c1e78105d3ff36f9c7247274
alexj <alexjvd@outlook.com>
2025-03-30 18:39:27 -0400

    Added delete (discard for outfits.py) functionality to both images.py and outfits.py. Added delete_outfit() to app.py which discards the outfit and deletes all images apart of the outfit.

--- src/images.py
+++ src/images.py
@@ -8,6 +8,9 @@ collection = mongo_connection.db["images"]
 # The collection name "users" will store all user data from the login page into the NOSTYLIST database
 user_collection = mongo_connection.db["users"]
 
+# The collection name "outfits" will store all outfit data into the NOSTYLIST database
+outfit_collection = mongo_connection.db["outfits"]
+
 
 # Convert image in codespace folder to binary, delete image in folder, returns image in binary format
 def image_to_binary():
@@ -54,15 +57,76 @@ def save_image(username, image_description, binary_data, outfit_num=0):
     collection.insert_one(document)
 
 
+# Only used to store the 4 stock images which will represent an "empty" clothing item a part of an outfit
+def save_stock_image(username, image_description, binary_data):  # DO NOT USE
+    image_description = image_description.lower()
+    # The admin user will only have 1 clothing item for each category (all blank images) shown in the user counts
+    user_collection.update_one(
+        {"username": username},
+        {"$set": {"num_" + image_description + "s": "1"}}
+    )
+
+    # Insert the image data and metadata into the database - the ID of a stock blank image is -1
+    document = {"username": username, "outfit_number": "0", "image_description": image_description,
+                "image_id": "-1", "image_data": binary_data}
+    collection.insert_one(document)
+
+
 # Returns the image data in a binary format, outfit_num is 0 if not attached to an outfit
 # Needs image_id but other retrieval methods can be made since we need a carousel feature
 def get_image(username, image_description, image_id, outfit_num=0):
     # Get the image in binary format from mongo and return it
-    result = collection.find_one(
-        {"username": username, "outfit_number": outfit_num, "image_description": image_description,
-         "image_id": image_id})
+    if image_id == "-1":
+        result = collection.find_one(  # admin is the username that contains all stock images - can change to whatever
+            {"username": "admin", "outfit_number": "0", "image_description": image_description,
+             "image_id": image_id})
+    else:
+        result = collection.find_one(
+            {"username": username, "outfit_number": outfit_num, "image_description": image_description,
+             "image_id": image_id})
     binary_data = result["image_data"]
     return binary_data
+
+
+# Delete an image, update all other image ids and number of images
+def delete_image(username, image_description, image_id, outfit_num=0):
+    # Delete image entry
+    collection.delete_one({"username": username, "outfit_number": outfit_num, "image_description": image_description,
+                           "image_id": image_id})
+
+    # If the image is a part of an outfit, the outfit will display a blank stock image
+    if outfit_num > 0:
+        # Update outfit collection
+        outfit_collection.update_one({"username": username, "outfit_number": outfit_num},
+                                     {"$set": {image_description + "_id": "-1"}})
+        # -1 will represent an id of blank stock clothing item image already stored in mongo
+
+    # Get previous number of image_descriptions e.g. number of hats, shirts, etc
+    num_image = int(get_num_image(username, image_description))
+
+    # Update all other image ids create after deleted image
+    for i in range(image_id + 1, num_image + 1):
+        num = i - 1
+        string_num = str(num)
+        collection.update_one({"username": username, "image_description": image_description, "image_id": str(i)},
+                              {"$set": {"image_id": string_num}})
+
+    # Decrement total number of image_descriptions e.g. number of hats, shirts, etc. in user collection
+    num_image -= 1
+    string_num = str(num_image)
+    user_collection.update_one({"username": username}, {"$set": {"num" + image_description + "s": string_num}})
+
+
+# Selection for which type of image count to get
+def get_num_image(username, image_description):
+    if image_description == "hat":
+        return get_num_hats(username)
+    elif image_description == "shirt":
+        return get_num_shirts(username)
+    elif image_description == "pant":
+        return get_num_pants(username)
+    else:
+        return get_num_shoes(username)
 
 
 # Return number of hats user has (string)
--- src/outfits.py
+++ src/outfits.py
@@ -6,6 +6,10 @@ collection = mongo_connection.db["outfits"]
 # The collection name "users" will store all user data from the login page into the NOSTYLIST database
 user_collection = mongo_connection.db["users"]
 
+# The collection name "images" will store all image data into the NOSTYLIST database
+image_collection = mongo_connection.db["images"]
+
+
 # The first outfit number is #1, if an image has an outfit #0 then it does not belong to an outfit
 # Same for all clothing / image ids
 
@@ -27,6 +31,7 @@ def create_outfit(username, outfit):
                 "pant_id": outfit[2], "shoe_id": outfit[3]}
     collection.insert_one(document)
 
+
 # Returns list of image descriptions and their respective image ids Ex: Hat, 1, Shirt, 2 etc.
 # Needs outfit_num but other retrieval methods can be made since we need a carousel feature
 def get_outfit(username, outfit_num):
@@ -36,16 +41,75 @@ def get_outfit(username, outfit_num):
     outfit.append(result["pant_id"]), outfit.append("shoe"), outfit.append(result["shoe_id"])
     return outfit
 
+
 # Updates all other outfit ids and clothing counts
-# Deletes an outfit based on username and outfit number
-def delete_outfit(username, outfit_num): ## DO NOT USE YET
-    # Need to change all user info and image info after delete
-    result = collection.delete_one({"username": username, "outfit_number": outfit_num})
+# Discards an outfit based on username and outfit number (all images are still saved in db but are now unlinked and have
+# an outfit number of 0
+# Returns all image ids (and previous outfit ID) so that if they want to delete the images they can
+def discard_outfit(username, outfit_num):
+    # Get all image and outfit ids
+    result = collection.find_one({"username": username, "outfit_number": outfit_num})
+
+    # Update deleted outfit numbers in image collection
+    for j in range(0, 4):
+        if j == 0:
+            image_description = "hat"
+            image_id = result["hat_id"]
+        elif j == 1:
+            image_description = "shirt"
+            image_id = result["shirt_id"]
+        elif j == 2:
+            image_description = "pant"
+            image_id = result["pant_id"]
+        else:
+            image_description = "shoe"
+            image_id = result["shoe_id"]
+        image_collection.update_one({"username": username, "image_description": image_description,
+                                     "image_id": image_id}, {"$set": {"outfit_number": "0"}})
+
+    # Delete outfit entry
+    collection.delete_one({"username": username, "outfit_number": outfit_num})
+
+    # Get number of outfits
+    num_outfits = int(get_num_outfits(username))
+
+    # Update all other outfit Ids created after
+    for i in range(outfit_num + 1, num_outfits + 1):
+        string_num2 = str(i)
+        result2 = collection.find_one({"username": username, "outfit_number": string_num2})
+        num = i-1
+        string_num = str(num)
+        collection.update_one({"username": username, "outfit_number": string_num2},
+                              {"$set": {"outfit_number": string_num}})
+
+        # Decrement outfit numbers in image collection
+        for j in range(0, 4):
+            if j == 0:
+                image_description = "hat"
+                image_id = result2["hat_id"]
+            elif j == 1:
+                image_description = "shirt"
+                image_id = result2["shirt_id"]
+            elif j == 2:
+                image_description = "pant"
+                image_id = result2["pant_id"]
+            else:
+                image_description = "shoe"
+                image_id = result2["shoe_id"]
+            image_collection.update_one({"username": username, "outfit_number": string_num2,
+                                         "image_description": image_description,"image_id": image_id},
+                                        {"$set": {"outfit_number": string_num}})
+
+    # Decrement number of outfits in user collection
+    num_outfits -= 1
+    string_num = str(num_outfits)
+    user_collection.update_one({"username": username}, {"$set": {"num_outfits": string_num}})
+
+    return result
+
 
 # Return number of outfits user has (string)
 def get_num_outfits(username):
     result = user_collection.find_one({"username": username})
     num = result["num_outfits"]
     return num
-
-
